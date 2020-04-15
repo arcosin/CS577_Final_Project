0f337cde07f01585631ced21dba256818e38189d
Maxwell J Jacobson <arcosin@users.noreply.github.com>
2020-04-15 17:32:33 -0500

    Fixed hypernym and added more templates.

--- build_hypernym_dataset.py
+++ build_hypernym_dataset.py
@@ -101,36 +101,61 @@ def buildIncorrectPairs(tree, goodPairs):
 def buildRecords(correctPairs, extraDatasets):
     correctRecords = []
     incorrectRecords = []
-    premise = Template("A $W1 is a $W2.")
-    hypothesis = Template("A $W1 is a $W2.")
+    premise = Template("There is not a $W in $L.")
+    hypothesis = Template("There is not a $W in $L.")
     for i, pair in enumerate(correctPairs):
-        w2, w1 = pair
-        pCor = premise.substitute(W1 = w1, W2 = w2)
-        hCor = hypothesis.substitute(W1 = w1, W2 = w2)
-        pInc = premise.substitute(W1 = w1, W2 = w2)
-        hInc = hypothesis.substitute(W1 = w2, W2 = w1)
+        hyper, hypo = pair
+        loc = random.choice(extraDatasets["location"])
+        pCor = premise.substitute(W = hyper, L = loc)
+        hCor = hypothesis.substitute(W = hypo, L = loc)
+        loc = random.choice(extraDatasets["location"])
+        pInc = premise.substitute(W = hypo, L = loc)
+        hInc = hypothesis.substitute(W = hyper, L = loc)
         correctRecords.append((pCor, hCor, True))
         incorrectRecords.append((pInc, hInc, False))
-    premise = Template("A $W1 is a $W2.")
-    hypothesis = Template("A $W1 is not necessarily a $W2.")
+    premise = Template("The $J only has a $W1 and a $W2.")
+    hypothesis = Template("The $J has two ${W1}s.")
     for i, pair in enumerate(correctPairs):
-        w2, w1 = pair
-        pCor = premise.substitute(W1 = w1, W2 = w2)
-        hCor = hypothesis.substitute(W1 = w2, W2 = w1)
-        pInc = premise.substitute(W1 = w1, W2 = w2)
-        hInc = hypothesis.substitute(W1 = w1, W2 = w2)
+        hyper, hypo = pair
+        job = random.choice(extraDatasets["jobs"])
+        pCor = premise.substitute(J = job, W1 = hyper, W2 = hypo)
+        hCor = hypothesis.substitute(J = job, W1 = hyper)
+        pInc = premise.substitute(J = job, W1 = hyper, W2 = hypo)
+        hInc = hypothesis.substitute(J = job, W1 = hypo)
         correctRecords.append((pCor, hCor, True))
         incorrectRecords.append((pInc, hInc, False))
     premise = Template("There is a $W in $L.")
     hypothesis = Template("There is a $W in $L.")
     for i, pair in enumerate(correctPairs):
-        w2, w1 = pair
+        hyper, hypo = pair
         loc = random.choice(extraDatasets["location"])
-        pCor = premise.substitute(W = w1, L = loc)
-        hCor = hypothesis.substitute(W = w2, L = loc)
+        pCor = premise.substitute(W = hypo, L = loc)
+        hCor = hypothesis.substitute(W = hyper, L = loc)
         loc = random.choice(extraDatasets["location"])
-        pInc = premise.substitute(W = w2, L = loc)
-        hInc = hypothesis.substitute(W = w1, L = loc)
+        pInc = premise.substitute(W = hyper, L = loc)
+        hInc = hypothesis.substitute(W = hypo, L = loc)
+        correctRecords.append((pCor, hCor, True))
+        incorrectRecords.append((pInc, hInc, False))
+    premise = Template("The $J purchased a $W.")
+    hypothesis = Template("The $J bought a $W.")
+    for i, pair in enumerate(correctPairs):
+        hyper, hypo = pair
+        job = random.choice(extraDatasets["jobs"])
+        pCor = premise.substitute(J = job, W = hypo)
+        hCor = hypothesis.substitute(J = job, W = hyper)
+        pInc = premise.substitute(J = job, W = hyper)
+        hInc = hypothesis.substitute(J = job, W = hypo)
+        correctRecords.append((pCor, hCor, True))
+        incorrectRecords.append((pInc, hInc, False))
+    premise = Template("The $J is living with a $W.")
+    hypothesis = Template("The $J has a $W in his home.")
+    for i, pair in enumerate(correctPairs):
+        hyper, hypo = pair
+        job = random.choice(extraDatasets["jobs"])
+        pCor = premise.substitute(J = job, W = hypo)
+        hCor = hypothesis.substitute(J = job, W = hyper)
+        pInc = premise.substitute(J = job, W = hyper)
+        hInc = hypothesis.substitute(J = job, W = hypo)
         correctRecords.append((pCor, hCor, True))
         incorrectRecords.append((pInc, hInc, False))
     return (correctRecords, incorrectRecords)
@@ -152,6 +177,7 @@ def main():
     dirname = getArgs()
     extraDatasets = dict()
     extraDatasets["location"] = getListDS(dirname + "locations.txt")
+    extraDatasets["jobs"] = getListDS(dirname + "jobs.txt")
     tree = buildTree(dirname + "hypernyms_tree.csv")
     goodPairs = buildCorrectPairs(tree)
     ent, nonEnt = buildRecords(goodPairs, extraDatasets)
