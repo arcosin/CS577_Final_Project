12fbe933331b454cd3869e3c3b16c4a0fdf9581b
Maxwell J Jacobson <arcosin@users.noreply.github.com>
2020-04-20 18:37:35 -0500

    Added more baselines.

--- build_baseline_dataset.py
+++ build_baseline_dataset.py
@@ -88,7 +88,44 @@ def buildRecords(ds, maxRecordsPerTemplate = 2000):
         incorrectRecords.append((pInc, hInc, False))
         hInc = hypothesis3.substitute(N = name, A = activity, L = location)
         incorrectRecords.append((pInc, hInc, False))
-        i += 3
+        i += 1
+        if i > maxRecordsPerTemplate:
+            break
+    premise = Template("The $J enjoys the $A1 and the $A2.")
+    hypothesis1 = Template("The $J likes the $A1.")
+    hypothesis2 = Template("The $J likes the $A2.")
+    hypothesis3 = Template("The $J enjoys the $A2 and the $A1.")
+    recTris = [(j, a1, a2) for j in ds["jobs"] for a1 in ds["animals"] for a2 in ds["animals"]]
+    i = 0
+    for job, a1, a2 in recTris:
+        if a1 != a2:
+            pCor = premise.substitute(J = job, A1 = a1, A2 = a2)
+            hCor = hypothesis1.substitute(J = job, A1 = a1)
+            correctRecords.append((pCor, hCor, True))
+            hCor = hypothesis2.substitute(J = job, A2 = a2)
+            correctRecords.append((pCor, hCor, True))
+            hCor = hypothesis3.substitute(J = job, A1 = a1, A2 = a2)
+            correctRecords.append((pCor, hCor, True))
+            i += 1
+            if i > maxRecordsPerTemplate:
+                break
+    premise = Template("$N is a $J with a pet $A and a house in $L.")
+    hypothesis1 = Template("The $J has a pet $A.")
+    hypothesis1 = Template("$N has a house in $L.")
+    hypothesis3 = Template("The $A is a $J.")
+    hypothesis4 = Template("$N has a pet $J.")
+    recQuads = [(n, j, a, l) for n in ds["names"] for j in ds["jobs"] for a in ds["animals"] for l in ds["location"]]
+    for n, j, a, l in recQuads:
+        pCor = premise.substitute(N = n, J = j, A = a, L = l)
+        hCor = hypothesis1.substitute(J = j, A = a)
+        correctRecords.append((pCor, hCor, True))
+        hCor = hypothesis2.substitute(N = n, L = l)
+        correctRecords.append((pCor, hCor, True))
+        hInc = hypothesis3.substitute(J = j, A = a)
+        incorrectRecords.append((pCor, hInc, False))
+        hInc = hypothesis3.substitute(N = n, J = j)
+        incorrectRecords.append((pCor, hInc, False))
+        i += 1
         if i > maxRecordsPerTemplate:
             break
     return (correctRecords, incorrectRecords)
