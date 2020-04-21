aeac8b73bf850e259769c7c0f8d3a9ac2980f8ff
Steven <sagallagher@mail.bradley.edu>
2020-04-21 14:23:01 -0400

    hierarchical temporal data building object and hierarchy

--- BuildDatasets.py
+++ BuildDatasets.py
@@ -3,6 +3,7 @@ from lib.BuildLocationalDataset import *
 from lib.BuildTemporalDataset import *
 from lib.BuildHypernymDataset import *
 from lib.BuildBaselineDataset import *
+from lib.BuildHierarchicalTemporalDataset import *
 
 BuildCompositionDataset("compositions.csv", "GeneratedDatasets/composition_entailment.txt","GeneratedDatasets/composition_nonentailment.txt","compositions")
 
@@ -11,5 +12,7 @@ BuildLocationalDataset("locations.csv", "GeneratedDatasets/locational_entailment
 BuildTemporalDataset("compositions.csv", "GeneratedDatasets/temporal_entailment.txt","GeneratedDatasets/temporal_nonentailment.txt", "temporal")
 
 BuildHypernymDataset("hypernyms_tree.csv", "GeneratedDatasets/hypernym_entailment.txt","GeneratedDatasets/hypernym_nonentailment.txt","compositions")
+
+BuildHierarchicalTemporalDataset("temporal.csv", "GeneratedDatasets/htemporal_entailment.txt","GeneratedDatasets/htemporal_nonentailment.txt", "compositions")
 
 #BuildBaselineDataset("compositions.csv", "GeneratedDatasets/baseline_entailment.txt","GeneratedDatasets/baseline_nonentailment.txt","compositions")
