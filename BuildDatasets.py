9aff9716e17782098c9abbbecb2efac709590746
Steven <sagallagher@mail.bradley.edu>
2020-04-22 12:39:10 -0400

    added function finalizeData to extract a random MAX_EXAMPLES from each Generated dataset and append them to a dataframe and write that dataframe to a csv

--- BuildDatasets.py
+++ BuildDatasets.py
@@ -4,15 +4,65 @@ from lib.BuildTemporalDataset import *
 from lib.BuildHypernymDataset import *
 from lib.BuildBaselineDataset import *
 from lib.BuildHierarchicalTemporalDataset import *
+import os
+import random
+import pandas as pd
 
-BuildCompositionDataset("compositions.csv", "GeneratedDatasets/composition_entailment.txt","GeneratedDatasets/composition_nonentailment.txt","compositions")
+def buildAll():
+    BuildCompositionDataset("compositions.csv", "GeneratedDatasets/composition_entailment.txt","GeneratedDatasets/composition_nonentailment.txt","compositions")
 
-BuildLocationalDataset("locations.csv", "GeneratedDatasets/locational_entailment.txt","GeneratedDatasets/locational_nonentailment.txt","compositions")
+    BuildLocationalDataset("locations.csv", "GeneratedDatasets/locational_entailment.txt","GeneratedDatasets/locational_nonentailment.txt","compositions")
 
-BuildTemporalDataset("compositions.csv", "GeneratedDatasets/temporal_entailment.txt","GeneratedDatasets/temporal_nonentailment.txt", "temporal")
+    BuildTemporalDataset("compositions.csv", "GeneratedDatasets/temporal_entailment.txt","GeneratedDatasets/temporal_nonentailment.txt", "temporal")
 
-BuildHypernymDataset("hypernyms_tree.csv", "GeneratedDatasets/hypernym_entailment.txt","GeneratedDatasets/hypernym_nonentailment.txt","compositions")
+    BuildHypernymDataset("hypernyms_tree.csv", "GeneratedDatasets/hypernym_entailment.txt","GeneratedDatasets/hypernym_nonentailment.txt","compositions")
 
-BuildHierarchicalTemporalDataset("temporal.csv", "GeneratedDatasets/htemporal_entailment.txt","GeneratedDatasets/htemporal_nonentailment.txt", "compositions")
+    BuildHierarchicalTemporalDataset("temporal.csv", "GeneratedDatasets/htemporal_entailment.txt","GeneratedDatasets/htemporal_nonentailment.txt", "compositions")
 
-BuildBaselineDataset("compositions.csv", "GeneratedDatasets/baseline_entailment.txt","GeneratedDatasets/baseline_nonentailment.txt","compositions")
+    BuildBaselineDataset("compositions.csv", "GeneratedDatasets/baseline_entailment.txt","GeneratedDatasets/baseline_nonentailment.txt","compositions")
+
+def finalizeData(MAX_EXAMPLES = 100):
+    result = pd.DataFrame(columns=['hypothesis','premis','label', 'category'])
+
+    # look through each file in generated datasets
+    for subdir, dirs, files in os.walk("GeneratedDatasets"):
+        for file_name in files:
+            # get the full file path
+            filepath = subdir + os.sep + file_name
+
+            # display which file were on to track progress
+            print(filepath)
+
+            # open the file
+            f = open(filepath, 'r')
+
+            # get number of lines in file
+            lines = f.readlines()
+
+            # if the number of lines is > MAX_EXAMPLES 
+            # then randomly choose MAX_EXAMPLE lines to keep in lines_to_append
+            while len(lines) > MAX_EXAMPLES:
+                del lines[random.randint(0,len(lines)-1)]
+
+            # category is the first part of the file name 
+            category = file_name.split('_')[0]
+
+            # loop through the lines to include 
+            for line in lines:
+                # strip special characters
+                line = line.strip('()\n\'\"')
+
+                # split hyp/prem/label
+                line = line.split(',')
+
+                # form our new row to append to result
+                new_row = {'hypothesis':line[0], 'premis':line[1], 'label':line[2], 'category':category}
+
+                # append the new row to result
+                result = result.append(new_row, ignore_index=True)
+
+    result.to_csv("GeneratedDatasets/final_data.csv")
+
+
+buildAll()
+finalizeData(100)
